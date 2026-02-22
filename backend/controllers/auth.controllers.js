41a6644352a9d33164cc8dd5349598d0bf3117fc
Meghraj-parashar <meghrajparashar@gmail.com>
2026-02-22 19:33:27 +0530

    tailwind

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcryptjs"
+import User from "../models/user.model.js"
+import generateToken from "../utils/generateToken.js"
+export const signUp = async (req,res)=>{
+    try {
+        const {fullName,email,password,mobile,role}=req.body
+        const user=await User.findOne({email})
+        if(user){
+            return res.status(400).json({message:"User already exist."})
+        }
+        if(password.length<6){
+            return res.status(401).json({message:"the password is less than 6"})
+        }
+        if(mobile.length<10){
+            return res.status(400).json({message:"the mobile number is less than 10 digits"})
+        }
+        const hasedPassword = await bcrypt.hash(password,10)
+        const newUser= await User.create({
+            fullName,
+            email,
+            password:hasedPassword,
+            mobile,
+            role
+        })
+        const token=generateToken(newUser._id)
+        res.status(201).json({
+            message:"User created successfully",
+            user:newUser
+        })
+        
+
+
+    } catch (error) {
+        console.log("error in signUp",error)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
