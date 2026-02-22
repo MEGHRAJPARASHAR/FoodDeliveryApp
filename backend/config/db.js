da3972364c9dc73843e6b9e72462edbdad0a8548
Meghraj-parashar <meghrajparashar@gmail.com>
2026-02-22 18:15:02 +0530

    1.>solved the dns problem 2.>made frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("db connect")
     } catch (error) {
-        console.log("db error")
+        console.log("DB ERROR \n",error)
     }
 }
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
-import express from 'express';
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);//my dns is creating problem, so using google dns
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 dotenv.config();
+import express from 'express';
+import connectDB from './config/db.js';
+
 const app = express();
-    
 const port = process.env.PORT || 5000;
-console.log("hi my name is meghraj")
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send("API is running....")
-})
-app.listen(port,()=>{
-    connectDB()
-    console.log(`Server is listening on port ${port}`)
-})
+});
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server is listening on port ${port}`);
+});
+
+//24 ->ecommerce exam and oops by java exam
+//25 -> .net and ai exam
