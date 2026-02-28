0a61053996fd77a67b1a7a1c406f68f35c022b10
Meghraj-parashar <meghrajparashar@gmail.com>
2026-02-28 20:52:44 +0530

    made SignIn.jsx and SignUp.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import SignUp from './PAGES/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <SignUp></SignUp>
+
+     
     </>
   )
 }
