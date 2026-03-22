b9ca0231a7d5b40b3df53c28eafb4b2aa88a143c
Meghraj-parashar <meghrajparashar@gmail.com>
2026-03-22 19:40:35 +0530

    made order model

---
+++ backend/models/order.model.js
@@ -0,0 +1,45 @@
+import mongoose, { mongo } from "mongoose"
+
+const orderItemSchema= new mongoose.Schema({
+    item:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Items",
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true
+    }
+})
+
+const orderSchema=new mongoose.Schema({
+    user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true   
+    },
+    shop:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Shop",
+        required:true
+    },
+    items:[orderItemSchema],
+    totalPrice:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        enum: ["pending", "confirmed", "preparing", "out_for_delivery", "delivered", "cancelled"],
+        default: "pending"
+    },
+    isPaid: { type: Boolean, default: false },
+    deliveryAddress: { type: String, required: true }
+},{timestamps:true})
+
+const Order= mongoose.model("Order", orderSchema)
+export default Order
