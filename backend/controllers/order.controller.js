0f8915f3d3fa403b6d823ee60e0bda097ed1e8e9
Meghraj-parashar <meghrajparashar@gmail.com>
2026-03-25 23:09:36 +0530

    made the create order function

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,4 +1,72 @@
 import Order from "../models/order.model.js"
+import Cart from "../models/cart.model.js"
+
+export const createOrder = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { deliveryAddress } = req.body;
+
+        // 1. Get user's cart
+        const cart = await Cart.findOne({ user: userId }).populate("items.item");
+
+        if (!cart || cart.items.length === 0) {
+            return res.status(400).json({ message: "Cart is empty" });
+        }
+
+        // 2. Ensure all items belong to same shop
+        const shopId = cart.items[0].item.shop.toString();
+
+        const isSameShop = cart.items.every(
+            ci => ci.item.shop.toString() === shopId
+        );
+
+        if (!isSameShop) {
+            return res.status(400).json({
+                message: "Items must be from same shop"
+            });
+        }
+
+        // 3. Prepare order items & calculate total
+        let totalPrice = 0;
+
+        const orderItems = cart.items.map(ci => {
+            const price = ci.item.price;
+
+            totalPrice += price * ci.quantity;
+
+            return {
+                item: ci.item._id,
+                quantity: ci.quantity,
+                price: price // snapshot
+            };
+        });
+
+        // 4. Create order
+        const order = await Order.create({
+            user: userId,
+            shop: shopId,
+            items: orderItems,
+            totalPrice,
+            deliveryAddress
+        });
+
+        // 5. Clear cart AFTER order success
+        cart.items = [];
+        await cart.save();
+
+        // 6. Send response
+        return res.status(201).json({
+            message: "Order placed successfully",
+            order
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        });
+    }
+};
 
 export const getAllOrders=async (req,res) => {
     try {
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getAllOrders, getOrderById } from '../controllers/order.controller';
+import { createOrder, getAllOrders, getOrderById } from '../controllers/order.controller';
+import { protectRoute } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 router.get('/', getAllOrders)
 router.get('/:id', getOrderById)
+router.get('/create-order',protectRoute, createOrder)
+
 
 export default router;
