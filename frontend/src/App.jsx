9664d519e0ee9aedaef0259983bde3316f5d9ab6
Meghraj-parashar <meghrajparashar@gmail.com>
2026-04-01 22:27:31 +0530

    if user isnt logged ,it will be redirected to signin, made protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SignUp from './pages/auth/SignUp'
 import SignIn from './pages/auth/SignIn'
+import ProtectedRoute from './components/ProtectedRoute'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<ProtectedRoute><h1>Home</h1></ProtectedRoute>} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
     </Routes>
